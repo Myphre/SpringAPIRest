4fffea92eac37b67aa02da0bf8f3743a80103323
ThalesVC <thalescristante@gmail.com>
2020-11-06 18:36:48 -0300

    Aula 04 - Trabalhando com POST

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,27 +1,38 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 //@Controller // permite mapeamento de chamadas pelo http pra chamada de uma funcao
 @RestController // assume por padrao que todos os metodos sao ResponseBoddy
+@RequestMapping("/topicos")
 public class TopicosController {
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
+	@Autowired
+	private CursoRepository cursoRepository;
 
-	@RequestMapping("/topicos")
+	@GetMapping
 	//@ResponseBody // Spring devolve o metodo diretamente, nao irei navegar pela pagina
 	
 	public List<TopicoDTO> lista(String nomeCurso){
@@ -34,6 +45,17 @@ public class TopicosController {
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 			return TopicoDTO.converter(topicos);
 		}
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriCompBuilder) { /// *Form, e pra pega info da tela/usuario e lancar pro programa. 
+		//o oposto do dto, que e pra pega do programa e manda pro usuario;
+		Topico topico = topicoForm.converter(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriCompBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
 		
 	}
 }
---
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,38 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
+public class TopicoForm {
+
+	private String titulo;
+	private String mensagem;
+	private String nomeCurso;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomecurso() {
+		return nomeCurso;
+	}
+	public void setNomecurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(this.nomeCurso);
+		return new Topico();
+		
+	}
+}
